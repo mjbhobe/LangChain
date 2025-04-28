1451b15183fcc8df582a1cf405c028bc6c88f330
Manish Bhobe <mjbhobe@gmail.com>
2025-04-28 13:44:20 +0000

    New code edits

---
+++ langchain_udemy/third_party/linkedin.py
@@ -0,0 +1,14 @@
+"""linkedin.py - scraping LinkedIn for profile info"""
+
+import os
+import requests
+from dotenv import load_dotenv
+
+# load all environment variables
+load_dotenv()
+
+
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
+    """scrape information from LinkedIn profiles using scrapein.io,
+    Manually scrape the information from profile page"""
+    pass
