45007a12a1dcfba3445b8ccc4bf2bb4d9120fc93
Manish Bhobe <mjbhobe@gmail.com>
2025-08-07 11:15:46 +0530

    Code edits with rich.console and rich.markdown for markdown text

--- gemini/hello_gemini.py
+++ gemini/hello_gemini.py
@@ -1,21 +1,25 @@
-""" hello_gemini.py: Hello Google Gemini"""
+"""
+hello_gemini.py: Hello Google Gemini
+This script demonstrates how to use the Google Gemini model as a chatbot
 
-import pathlib
-import textwrap
+@author: Manish Bhobe
+My experiments with Python, ML, Gen AI, and more
+Code shared for educational purposes only. Use at your own risk!!
+"""
+
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
+from rich.console import Console
+from rich.markdown import Markdown
 
 import google.generativeai as genai
 
-# Used to securely store your API key
-# from google.colab import userdata
-
-from IPython.display import display
-from IPython.display import Markdown
-
 # load env variables from .env file
-_ = load_dotenv(find_dotenv())  # read local .env file
+_ = load_dotenv(override=True)  # read local .env file
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+
+# for colorful output
+console = Console()
 
 
 def to_markdown(text):
@@ -25,25 +29,33 @@ def to_markdown(text):
 
 
 def main():
-    # list all available models
+    # list all available Google models
     for m in genai.list_models():
         if "generateContent" in m.supported_generation_methods:
             print(m.name)
 
-    # we'll use the only available model
-    llm = genai.GenerativeModel("gemini-pro")
+    # we'll be using the gemini-2.5-flash model
+    # you can choose any model from the list displayed above
+    # if you want to use a different model, change the model name below
+    llm = genai.GenerativeModel("gemini-2.5-flash")
     prompt_text = ""
+    console.print(
+        Markdown(
+            "Enter your question below **or** type _quit_ or _bye_ or _exit_ to end the chat",
+            style="blue",
+        )
+    )
     while True:
-        prompt_text = input("Enter your question: ")
-        if prompt_text.lower() == "quit":
+        console.print("[green]You? [/green]", end="")
+        prompt_text = input()
+        if prompt_text.lower() in ["bye", "quit", "exit"]:
             break
         try:
             response = llm.generate_content(prompt_text)
-            print("\n")
-            print(response.text)
-            print("-" * 80)
+            console.print("[yellow]AI: [/yellow]")
+            console.print(Markdown(response.text))
         except ValueError as err:
-            print(f"Error: {err}")
+            console.print(f"[red]Error:[/red]\n{err}")
             print(f"{response.prompt_feedback}")
 
 
--- langchain_tutorial/simple_llm_with_lcel.py
+++ langchain_tutorial/simple_llm_with_lcel.py
@@ -1,12 +1,13 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 from rich import print
 
 # load API keys from local .env file
-load_dotenv(find_dotenv())
+load_dotenv(override=True)
+
 
 def get_model():
-    # create LLM based on value of LLM_TO_USE
+    # create LLM based on value of LLM_TO_USE in .env file
     if os.environ["LLM_TO_USE"] == "openai":
         from langchain_openai import ChatOpenAI
 
@@ -16,52 +17,18 @@ def get_model():
         from langchain_google_genai import ChatGoogleGenerativeAI
 
         model = ChatGoogleGenerativeAI(
-            model="gemini-1.5-flash",
+            model="gemini-2.5-flash",
             temperature=0,
             max_tokens=None,
             timeout=None,
             max_retries=2,
             # other params...
         )
-        print("Using ChatGoogleGenerativeAI -> gemini-1.5-flash model")
+        print("Using ChatGoogleGenerativeAI -> gemini-2.5-flash model")
     return model
+
 
 model = get_model()
 response = model.invoke("Hello World!")
 print("Response:")
 print(response)
-
-# from langchain_core.messages import HumanMessage, SystemMessage
-# from langchain_core.output_parsers import StrOutputParser
-#
-# SYSTEM_MESSAGE = "Translate the following from English into French"
-# USER_MESSAGE = "I love the Langchain framework!"
-#
-# messages = [
-#     SystemMessage(content=SYSTEM_MESSAGE),
-#     HumanMessage(content=USER_MESSAGE),
-# ]
-#
-# print(SYSTEM_MESSAGE)
-# print(USER_MESSAGE)
-#
-# result = model.invoke(messages)
-# parser = StrOutputParser()
-# print(f">> {parser.invoke(result)}")
-#
-# print("Using LCEL instead")
-# chain = model | parser
-#
-# print(SYSTEM_MESSAGE)
-# print(USER_MESSAGE)
-# print(f">> {chain.invoke(messages)}")
-#
-# # using prompt template
-# from langchain_core.prompts import ChatPromptTemplate
-#
-# system_template = "Translate the following from English into {language}"
-# prompt_template = ChatPromptTemplate.from_messages(
-#     [("system", system_template), ("user", "{text}")]
-# )
-# result = prompt_template.invoke({"language": "French", "text": USER_MESSAGE})
-# print(f">> {result.to_messages()}")
